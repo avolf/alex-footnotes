4004cd3c7848d087935d8b7839d4223a3e0ca852
defomicron <defomicron@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-08-03 23:40:18 +0000

    1.3 update
    git-svn-id: https://plugins.svn.wordpress.org/civil-footnotes/trunk@750846 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- footnotes.php
+++ footnotes.php
@@ -1,10 +1,10 @@
 <?php
 /*
 Plugin Name: Civil Footnotes
-Plugin URI: https://defomicron.net/projects/civil_footnotes/
+Plugin URI: https://defomicron.net/projects/civil_footnotes
 Version: 1.3
 Description: Parses and displays footnotes. Based on <a href="http://elvery.net/drzax/wordpress-footnotes-plugin">WP-Foonotes</a> by <a href="http://elvery.net">Simon Elvery</a>, and the footnote syntax pioneered by <a href="http://daringfireball.net/2005/07/footnotes">John Gruber</a>.
-Author: <a href="https://defomicron.net/colophon/">Austin Sweeney</a>
+Author: <a href="https://defomicron.net/colophon">Austin Sweeney</a>
 */
 
 // If you’d like to edit the output, scroll down to the
